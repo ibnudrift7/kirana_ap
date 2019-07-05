c854703de24334fbfbd876a190b13d4379c119fc
Ghozi Akbar <ghoziakbar9@gmail.com>
2019-07-05 16:45:25 +0700

    page kontak

---
+++ protected/views/home/kontak.php
@@ -0,0 +1,146 @@
+<section class="kontak-sec-1">
+    <div class="prelative1 container">
+        <div class="row">
+            <div class="col-md-60 p-0">
+                <div class="title">
+                    <p>HUBUNGI KAMI</p>
+                </div>
+                <div class="prelative2 subtitle">
+                    <h2>Tim layanan pelanggan kami akan senantiasa
+                        siap untuk melayani segala pertanyaan anda</h2>
+                </div>
+                <div class="img1">
+                    <img src="<?php echo $this->assetBaseurl; ?>ijo.png" alt="">
+                </div>
+                <div class="logo pt-5">
+                    <img src="<?php echo $this->assetBaseurl; ?>kirana.jpg" alt="">
+                </div>
+                <div class="title-address pt-5">
+                    <p>Warehouse / Headquarter</p>
+                </div>
+                <div class="address pt-4 pb-5">
+                    <p>PT. Kirana Food<br>
+                        Kawasan Industri Pulo Gadung, Blok DD No. 12<br>
+                        Jl. Rw. Sumur III, Jatinegara, Kec. Cakung<br>
+                        Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13930<br>
+                        Indonesia</p>
+                </div>
+            </div>
+        </div>
+        <div class="prelative2 row">
+            <div class="col-md-19">
+                <div class="box-content ">
+                    <img class="w-40 pt-4 pb-4" src="<?php echo $this->assetBaseurl; ?>phone.png" alt="Card image cap">
+                    <div class="content-text  text-center">
+                        <div class="contact-title">
+                            <p>Telephone</p>
+                        </div>
+                        <div class="contact-detail">
+                            <p>+62 21 5887 8585</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="vl"></div>
+            <div class="col-md-19">
+                <div class="box-content ">
+                    <img class="w-40 pt-4 pb-4" src="<?php echo $this->assetBaseurl; ?>mail.png" alt="Card image cap">
+                    <div class="content-text  text-center">
+                        <div class="contact-title">
+                            <p>Telephone</p>
+                        </div>
+                        <div class="contact-detail">
+                            <p>+62 21 5887 8585</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="vl"></div>
+            <div class="col-md-19 p-0">
+                <div class="box-content ">
+                    <img class="w-40 pt-4 pb-3" src="<?php echo $this->assetBaseurl; ?>walogo.png" alt="Card image cap">
+                    <div class="content-text  text-center">
+                        <div class="contact-title">
+                            <p>Telephone</p>
+                        </div>
+                        <div class="contact-detail">
+                            <p>+62 21 5887 8585</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="kontak-sec-2">
+    <div class="prelative1 container">
+        <div class="row">
+            <div class="col-md-60 p-0">
+                <div class="title pt-5">
+                    <p>ANDA BUTUH BANTUAN ATAU INFORMASI LEBIH?</p>
+                </div>
+                <div class="prelative2 subtitle pt-4">
+                    <h3>Mari berbicara dengan tim kami di Kirana Food.<br>
+                        Kami akan menyambut semua pertanyaan dan kebutuhan anda!</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row form pt-5">
+            <div class="col-md-15">
+                <label class="text">NAMA PERUSAHAAN</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="col-md-15">
+                <label class="text">NAMA</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="col-md-15">
+                <label class="text">TELEPON</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="col-md-15">
+                <label class="text">EMAIL</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+        </div>
+        <div class="row form pt-4">
+            <div class="col-md-60">
+                <p class="text">TERTARIK UNTUK MENGETAHUI TENTANG</p>
+                <div class="form-check-inline pt-3">
+                    <label class="form-check-label">
+                        <input type="radio" class="form-check-input radiob" name="optradio">KENTANG GORENG / POTATO FRIES
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label">
+                        <input type="radio" class="form-check-input radiob" name="optradio">NUGGETS & PROCESSED MEATS
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label">
+                        <input type="radio" class="form-check-input radiob" name="optradio" disabled>FROZEN VEGETABLES
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label class="form-check-label">
+                        <input type="radio" class="form-check-input radiob" name="optradio" disabled>LAINNYA
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="row form pt-5">
+            <div class="col-md-30">
+                <img class="" src="<?php echo $this->assetBaseurl; ?>Layer45.png" alt="Card image cap">
+            </div>
+            <div class="col-md-30">
+                <button class="submit">SUBMIT</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="kontak-sec-3">
+    <div class="container">
+    </div>
+</section>
