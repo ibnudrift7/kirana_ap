b9cf7833a64259ffc3ab7a5b0ff89929ed1ee95c
Ghozi Akbar <ghoziakbar9@gmail.com>
2019-07-07 21:57:32 +0700

    page produk

--- protected/views/home/produk.php
+++ protected/views/home/produk.php
@@ -1,201 +1,121 @@
-<section class="cover">
-    <div class="container cont-fcs produk">
-        <div class="cover-image">
-            <img class="w-100 d-block" src="<?php echo $this->assetBaseurl; ?>cover-produk_03<?php echo $i ?>.jpg" alt="">
-            <div class="centered">
-              <p>
-                <?php echo strtoupper( Tt::t('front', 'Calcium Carbonate Products') ); ?>
-              </p>
-            </div>
-        </div>
-        <div class="row pt-3">
-          <div class="col-40">
-            <div class="breadcrumb">
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">HOME</a></li>
-                  <li class="breadcrumb-item"><a href="#"><?php echo strtoupper( Tt::t('front', 'Calcium Carbonate Products') ); ?></a></li>
-                </ol>
-              </nav>
-            </div>
-          </div>
-          <div class="col-20">
-            <div class="back text-right">
-              <a href="#"><?php echo strtoupper( Tt::t('front', 'BACK') ); ?></a>
-            </div>
-          </div>
-        </div>
-        <hr class="cover">
-    </div>
-</section>
-<section class="cover-above">
-    <div class="prelative container">
-        <div class="row pt-5">
-            <?php if (Yii::app()->language == 'en'): ?>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text1 mx-auto d-block text-center pb-4">
-                        <p>our product as part of your operational</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text2 mx-auto d-block text-center pb-4">
-                        <p>Our Calcium Carbonate With You In Mind</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text3 mx-auto d-block text-center pb-4">
-                        <p>We take pride in providing high quality calcium carbonate products to our market in Surabaya, Jakarta and throuhout Indonesia. We will meet your specification and we will ensure a steady production flow by maintaining our stock priority for your long-term.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text4 mx-auto d-block text-center pb-4">
-                        <p>Calcium Carbonate is an essential material for the producing of paint industries, chemical industries, ceramics industries, plastic industries, paper industries and so much more. A quality calcium carbonate that meets the requirements will ensure the smooth operation and production of our customers factory. As an important and integral part of the production, calcium carbonate came at a very low price - therefore there are no reasons or options for our customers to choose a lower grade of calcium carbonate. “With you in mind” as our everyday spirit and commitment, we produce the best product possible to guarantee your success.</p>
-                    </div>
-                </div>
-            </div>
-            <?php else: ?>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text1 mx-auto d-block text-center pb-4">
-                        <p>produk kami sebagai bagian dari operasional Anda</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text2 mx-auto d-block text-center pb-4">
-                        <p>Kalsium Karbonat Kami Dengan Anda Dalam Pikiran</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text3 mx-auto d-block text-center pb-4">
-                        <p>Kami bangga menyediakan produk kalsium karbonat berkualitas tinggi untuk pasar kami di Surabaya, Jakarta dan di seluruh Indonesia. Kami akan memenuhi spesifikasi Anda dan kami akan memastikan aliran produksi yang stabil dengan mempertahankan prioritas stok kami untuk jangka panjang Anda.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-60">
-                <div class="caption">
-                    <div class="text4 mx-auto d-block text-center pb-4">
-                        <p>Kalsium Karbonat adalah bahan penting untuk memproduksi industri cat, industri kimia, industri keramik, industri plastik, industri kertas dan banyak lagi. Kalsium karbonat berkualitas yang memenuhi persyaratan akan memastikan kelancaran operasi dan produksi pabrik pelanggan kami. Sebagai bagian penting dan integral dari produksi, kalsium karbonat dijual dengan harga yang sangat rendah - oleh karena itu tidak ada alasan atau pilihan bagi pelanggan kami untuk memilih kadar kalsium karbonat yang lebih rendah. “Dengan pertimbangan Anda” sebagai semangat dan komitmen kami sehari-hari, kami menghasilkan produk terbaik untuk menjamin kesuksesan Anda.</p>
-                    </div>
-                </div>
-            </div>
-            <?php endif; ?>
-        </div>
-    </div>
-    <div class="pb-5"></div>
-    <div class="pb-3"></div>
-    <!-- <div class="pb-5"></div> -->
-</section>
-
 <section class="produk-sec-1">
     <div class="prelative container pt-5">
         <div class="row">
-            <div class="col-md-60">
-                <div class="title pt-5 mx-auto d-block text-center">
-                    <?php if (Yii::app()->language == 'en'): ?>
-                    <h3>our product variety</h3>
-                    <?php else: ?>
-                    <h3>VARIASI PRODUK KAMI</h3>
-                    <?php endif; ?>
+            <div class="col-md-60 pb-5">
+                <div class="pt-5 mx-auto d-block text-center">
+                    <h4 class="title1">Produk</h4>
+                </div>
+                <div class="pt-3 mx-auto d-block text-center">
+                    <h3 class="subtitle">Temukan produk makanan beku / frozen food<br>
+                        yang lezat, terjamin dan higienis</h3>
+                </div>
+                <div class="pt-5 mx-auto d-block text-center">
+                    <p class="text">Saat ini, kita berada di sebuah era di mana kreasi makanan dan teknologi di dunia makanan berkembang dengan sangat pesat. Berbagai varian / jenis makanan terus menerus dikembangkan dan PT. Kirana Food ada di dekat anda untuk memastikan bahwa ketika anda membeli produk pilihan kami, anda tidak hanya merasa nikmat dan lezat, namun dapat dipastikan anda mengkonsumsi produk yang halal, sehat, higinis dan diproses dengan baik.
+                    </p>
+                </div>
+                <div class="pt-5 mx-auto d-block text-center">
+                    <img src="<?php echo $this->assetBaseurl; ?>Layer-26.png" alt="">
                 </div>
             </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
-                    </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type A</h3>
-                    </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
-                    </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>Rectangle1<?php echo $i ?>.png" alt="">
                 </div>
             </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <div class="content pl-5 pt-3 pr-5">
+                        <div class="pt-4">
+                            <h3 class="titlecard">Produk Kentang Beku / Frozen Potato</h3>
+                        </div>
+                        <div class="pt-2">
+                            <p class="text">Aneka varian produk kentang beku impor berkualitas, dengan berbagai bentuk dan bumbu saji yang membuat hidangan sangat mudah diproses dan dinikmati.</p>
+                        </div>
                     </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type B</h3>
-                    </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
+                    <div class="cardfooter d-table pl-5">
+                        <p class="textfooter d-table-cell align-middle">Lihat varian produk</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <div class="content pl-5 pt-3 pr-5">
+                        <div class="pt-4">
+                            <h3 class="titlecard">Produk Sayuran Beku / Frozen
+                                Vegetables</h3>
+                        </div>
+                        <div class="pt-2">
+                            <p class="text">Aneka varian produk sayuran beku impor berkualitas seperti kacang polong, jagung, wortel, jamur, asparagus, brokoli dan masih banyak lagi, untuk melengkapi hidangan utama anda.</p>
+                        </div>
                     </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type C</h3>
-                    </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
-                    </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type A</h3>
-                    </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
+                    <div class="cardfooter d-table pl-5">
+                        <p class="textfooter d-table-cell align-middle">Lihat varian produk</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>Rectangle1<?php echo $i ?>.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>Rectangle1<?php echo $i ?>.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <div class="content pl-5 pt-3 pr-5">
+                        <div class="pt-4">
+                            <h3 class="titlecard">Produk Buah Beku / Frozen Fruits</h3>
+                        </div>
+                        <div class="pt-2">
+                            <p class="text">Buah segar yang telah dikeringkan dan siap digunakan sebagai bahan baku pendukung masakan anda.</p>
+                        </div>
                     </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type B</h3>
-                    </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
+                    <div class="cardfooter d-table pl-5">
+                        <p class="textfooter d-table-cell align-middle">Lihat varian produk</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-20">
-                <div class="box-produk pt-5">
-                    <div class="img pt-3">
-                        <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>design-1-products_03<?php echo $i ?>.jpg" alt="">
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <div class="content pl-5 pt-3 pr-5">
+                        <div class="pt-4">
+                            <h3 class="titlecard">Produk Makanan Proses / Processed
+                                Food</h3>
+                        </div>
+                        <div class="pt-2">
+                            <p class="text">Aneka produk makanan proses siap dimasak seperti daging olahan berbentuk nugget, lumpia, samosa, fish cake, siomay dan masih banyak lagi.</p>
+                        </div>
                     </div>
-                    <div class="title pt-4">
-                        <h3>Calcium Carbonate Type C</h3>
+                    <div class="cardfooter d-table pl-5">
+                        <p class="textfooter d-table-cell align-middle">Lihat varian produk</p>
                     </div>
-                    <div class="read">
-                        <p>
-                            <a href="#"><?php echo Tt::t('front', 'Read More'); ?></a>
-                        </p>
+                </div>
+            </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>Rectangle1<?php echo $i ?>.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <img class="img img-fluid w-100" src="<?php echo $this->assetBaseurl; ?>Rectangle1<?php echo $i ?>.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-30 pt-4 px-0">
+                <div class="box-produk">
+                    <div class="content pl-5 pt-3 pr-5">
+                        <div class="pt-4">
+                            <h3 class="titlecard">Produk lainnya</h3>
+                        </div>
+                        <div class="pt-2">
+                            <p class="text">Berbagai pengembangan usaha PT. Kirana Food lainnya seperti daging sapi beku, ayam beku, udang beku, dan masih banyak lagi.</p>
+                        </div>
+                    </div>
+                    <div class="cardfooter d-table pl-5">
+                        <p class="textfooter d-table-cell align-middle">Lihat varian produk</p>
                     </div>
                 </div>
             </div>
