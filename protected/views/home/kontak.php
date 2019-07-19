069da73d3cfa899b8882748e49c43e017c76a8d4
ibnu fajar <ibnudrift@gmail.com>
2019-07-19 10:19:36 +0700

    upd kontak

--- protected/views/home/kontak.php
+++ protected/views/home/kontak.php
@@ -136,6 +136,9 @@
 
 <section class="kontak-sec-3">
     <div class="mapouter">
-        <div class="gmap_canvas"><iframe width="100%" height="588" id="gmap_canvas" src="https://maps.google.com/maps?q=The%20Messenger%20Cafe&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
+        <div class="gmap_canvas">
+            <iframe width="100%" height="588" id="gmap_canvas" src="https://maps.google.com/maps?q=PT%20Kirana%20Food&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <div class="clear"></div>
+        </div>
     </div>
 </section>
