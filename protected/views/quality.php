eab0ade6a6945016869753a463d5849dcff01069
ibnu fajar <ibnudrift@gmail.com>
2019-07-29 10:03:04 +0700

    updates n style

---
+++ protected/views/quality.php
@@ -0,0 +1,181 @@
+<section class="cover-inside quality">
+  <div class="prelative container">
+    <div class="row">
+      <div class="col-md-60">
+        <div class="content">
+          <div class="title">
+            <p>KUALITAS</p>
+          </div>
+          <div class="subtitle">
+            <p>Lezat, sehat dan higienis adalah standar kualitas yang tidak dapat ditoleransi</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="quality-sec-1">
+  <div class="prelative container">
+    <div class="row">
+      <div class="col-md-60">
+        <p class="subtitle">PROSES KUALITAS</p>
+      </div>
+      <div class="col-md-30">
+        <div class="content-sec-1">
+          <div class="title">
+            <p>Apapun yang kami lakukan, adalah berpusat pada betapa lezat dan sempurnanya produk saat dikonsumsi oleh pelanggan akhir.</p>
+        </div>
+          <div class="isi">
+            <p>Proses untuk memastikan produk makanan PT. Kirana Food berkualitas dimulai dari tahapan sangat awal. Yakni bermula dari melihat siapa prinsipal / pemasok kami. Sederet syarat sertifikasi HACCP, ISO 9001, ISO 14001 dan sertifikat makanan Halal adalah standar yang dimiliki oleh pemasok rekan PT. Kirana Food. PT. Kirana Food lalu tidak semata puas dengan pernyataan sertifikasi, hal ini ditindak lanjuti dengan kunjungan ke proses produksi dari tempat pengolahan / fasilitas dari prinsipal untuk memastikan kebenaran sertifikat serta pembuktian proses yang berkualitas.</p>
+            <p>Setelah produk dari prinsipal sampai di gudang pemrosesan PT. Kirana Food, proses pemeriksaan kualitas pun dilakukan per batch dari kiriman untuk kemudian dilakukan proses lebih lanjut seperti pengemasan dan penyimpanan yang memperhatikan kesegaran, kebersihan dan keawetan produk, hingga sampai pada proses muat ke armada PT. Kirana Food yang berpendingin agar sampai di tangan rekan dan jaringan distribusi dalam kondisi prima tanpa kerusakan dan penurunan kualitas.</p>
+            <p>Adalah kolaborasi antara sistem kualitas dan kedisiplinan dalam pelaksanaan prosedur yang memampukan PT. Kirana Food mempersembahkan produk frozen food dengan sempurna.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-30">
+        <div class="content-sec-2 d-block mx-auto quality-sc-1_right">
+          <div class="row mx-auto">
+            <div class="atas col-md-60">
+              <img src="<?php echo $this->assetBaseurl; ?>atas.png" alt="">
+            </div>
+            <div class="col-md-60">
+              <img src="<?php echo $this->assetBaseurl; ?>piramida.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="quality-download">
+  <div class="prelative container">
+    <div class="row">
+      <div class="col-md-60">
+        <div class="title">
+          <p>SERTIFIKAT KAMI</p>
+        </div>
+        <div class="subtitle">
+          <p>Komitmen kami terhadap kualitas </p>
+        </div>
+      </div>
+      <div class="col-md-20">
+
+      </div>
+      <div class="col-md-20">
+        <div class="image"><img class="w-100" src="<?php echo $this->assetBaseurl; ?>Tuv Nord (sample iso).jpg" alt=""></div>
+        <div class="download">
+          <a href="<?php echo $this->assetBaseurl; ?>Certificate HACCP - Kirana Food.pdf" download="filename">
+            Click To Download
+          </a>
+        </div>
+      </div>
+      <div class="col-md-20">
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="quality-sec-2">
+  <div class="prelative container">
+    <div class="row">
+      <div class="col-md-60">
+        <div class="title">
+          <p>Proses Berkualitas Dengan Tenaga Kerja Dan Infrastruktur Terbaik</p>
+        </div>
+        <div class="subtitle">
+          <p>Telah bertahun-tahun PT. Kirana Food mampu menembus pasar nasional Indonesia yang sangat berkompetisi dan memiliki kompetitor produk sejenis dengan varian yang sangat banyak. Keberhasilan kami tersebut juga berkat sumbangsih dari kehandalan dalam penanganan hulu ke hilir distribusi, yakni dari pengemasan yang rapi, pengangkutan muatan yang aman, pendokumentasian yang teliti, serta transportasi dan logistik yang menjaga kualitas dan rasa dengan prima.</p>
+        </div>
+      </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-1.jpg" alt=""></div>
+        </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-2.jpg" alt=""></div>
+        </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-3.jpg" alt=""></div>
+        </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-4.jpg" alt=""></div>
+        </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-5.jpg" alt=""></div>
+        </div>
+        <div class="col-md-20">
+          <div class="frame"><img src="<?php echo $this->assetBaseurl; ?>kualitas-section-2-6.jpg" alt=""></div>
+        </div>
+
+      
+    </div>
+  </div>
+</section>
+
+<section class="home-sec-3">
+	<div class="prelative container">
+		<div class="row">
+			<div class="col-md-60">
+				<div class="box-content">
+					<div class="content">
+						<div class="title">
+							<p>ANDA BUTUH BANTUAN ATAU INFORMASI LEBIH?</p>
+						</div>
+						<div class="subtitle">
+							<p>Mari berbicara dengan tim kami di Kirana Food. Kami akan menyambut semua pertanyaan dan kebutuhan anda!</p>
+						</div>
+						<form class="fieldd">
+							<div class="form-row">
+								<div class="form-group col-md-15">
+									<label for="inputEmail4">Nama Perusahaan</label>
+									<input type="text" class="form-control" id="" placeholder="">
+								</div>
+								<div class="form-group col-md-15">
+									<label for="inputEmail4">Nama</label>
+									<input type="text" class="form-control" id="" placeholder="">
+								</div>
+								<div class="form-group col-md-15">
+									<label for="inputEmail4">Telepon</label>
+									<input type="nama" class="form-control" id="inputEmail4" placeholder="">
+								</div>
+								<div class="form-group col-md-15">
+									<label for="inputEmail4">Email</label>
+									<input type="email" class="form-control" id="inputEmail4" placeholder="">
+								</div>
+							</div>
+						</form>
+						<div class="tertarik">
+							<p>TERTARIK UNTUK MENGETAHUI TENTANG</p>
+						</div>
+						<form class="radioo">
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+								<label class="form-check-label" for="inlineRadio1">KENTANG GORENG / POTATO FRIES</label>
+							</div>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+								<label class="form-check-label" for="inlineRadio2">NUGGETS & PROCESSED MEATS</label>
+							</div>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+								<label class="form-check-label" for="inlineRadio1">FROZEN VEGETABLES</label>
+							</div>
+							<div class="form-check form-check-inline">
+								<input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+								<label class="form-check-label" for="inlineRadio2">LAINNYA</label>
+							</div>
+						</form>
+						<button class="form-control">submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style type="text/css">
+  section.home-sec-3{
+    background: url(<?php echo Yii::app()->baseUrl; ?>/asset/images/bottom-backsedit2_02.jpg) no-repeat;
+  }
+</style>
